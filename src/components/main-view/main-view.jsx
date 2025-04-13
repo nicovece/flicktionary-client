0df07624523341dc4661d3b6277d59dfb35b4888
Nico Vece <me@nicovece.com>
2025-04-13 19:00:48 +0200

    Installed dependencies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,7 @@ import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { SignupView } from '../signup-view/signup-view';
 import { Row, Col, Navbar, Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // MainView component
 const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
